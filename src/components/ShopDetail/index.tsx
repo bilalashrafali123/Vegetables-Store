ab36efd3c431531073e2402d266444c24d38a9bd
bilalashrafali123 <ashrafalibilal385@gmail.com>
2025-05-23 11:17:43 +0500

    shop-page-completed...

--- src/components/ShopDetail/index.tsx
+++ src/components/ShopDetail/index.tsx
@@ -1,3 +1,4 @@
+// import Footer from '../Footer'
 import Header from '../Header'
 import Navbar from '../NavBar'
 import ShopBanner from '../ShopBanner'
@@ -8,6 +9,7 @@ const ShopDetail = () => {
     <Header />
     <Navbar />
     <ShopBanner title='ShopDetail' title2='ShopDetail' />
+    {/* <Footer /> */}
     </>
   )
 }
