4f1af30f40c5f0a9a4bc5ec807984433ecd78f6b
bilalashrafali123 <ashrafalibilal385@gmail.com>
2025-05-25 03:32:52 +0500

    updated...

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -1,4 +1,3 @@
-// import Footer from "../Footer"
 import Footer from "../Footer";
 import Header from "../Header";
 import Navbar from "../NavBar";
@@ -11,6 +10,7 @@ const Cart = () => {
       <Header />
       <Navbar />
       <ShopBanner title="Cart" title2="Cart" />
+
       <div className="container">
         <table className="table">
           <thead>
@@ -33,34 +33,33 @@ const Cart = () => {
                   />
                 </div>
               </th>
-              <td className="table-data ">Big Banana</td>
+              <td className="table-data">Big Banana</td>
               <td className="table-data">2.99 $</td>
-              <td className="table-data">2.99 $</td>
-              <td className="table-data">2.99 $</td>
+              <td className="table-data">2</td>
+              <td className="table-data">5.98 $</td>
               <td className="table-data">
                 <button>X</button>
               </td>
             </tr>
-          </tbody>
-          <tr>
-            <th scope="row">
-              <div className="table-wrapper">
-                <img
-                  src="https://demo.htmlcodex.com/2824/vegetable-website-template/img/vegetable-item-5.jpg"
-                  alt="img"
-                />
-              </div>
-            </th>
-            <td className="table-data ">Potato</td>
-            <td className="table-data">2.99 $</td>
-            <td className="table-data">2.99 $</td>
-            <td className="table-data">2.99 $</td>
-            <td className="table-data">
-              <button>X</button>
-            </td>
-          </tr>
 
-          <tbody>
+            <tr>
+              <th scope="row">
+                <div className="table-wrapper">
+                  <img
+                    src="https://demo.htmlcodex.com/2824/vegetable-website-template/img/vegetable-item-5.jpg"
+                    alt="img"
+                  />
+                </div>
+              </th>
+              <td className="table-data">Potato</td>
+              <td className="table-data">1.50 $</td>
+              <td className="table-data">3</td>
+              <td className="table-data">4.50 $</td>
+              <td className="table-data">
+                <button>X</button>
+              </td>
+            </tr>
+
             <tr>
               <th scope="row">
                 <div className="table-wrapper">
@@ -70,24 +69,54 @@ const Cart = () => {
                   />
                 </div>
               </th>
-              <td className="table-data ">Brocoli</td>
-              <td className="table-data">2.99 $</td>
-              <td className="table-data">2.99 $</td>
-              <td className="table-data">2.99 $</td>
+              <td className="table-data">Broccoli</td>
+              <td className="table-data">3.00 $</td>
+              <td className="table-data">2</td>
+              <td className="table-data">6.00 $</td>
               <td className="table-data">
                 <button>X</button>
               </td>
             </tr>
           </tbody>
         </table>
-         <div className="coupon">
-         <input type="text" placeholder="Coupon Code" />
-         <button>Apply Coupon</button>
+
+        <div className="coupon">
+          <input type="text" placeholder="Coupon Code" />
+          <button>Apply Coupon</button>
+        </div>
+
+        <div className="cart-section">
+          <div className="cart-background">
+            <div className="cart-text-section">
+              <h1>Cart Total</h1>
+              <div className="subtotal">
+                <h5>Subtotal:</h5>
+                <h5>$16.48</h5>
+              </div>
+              <div className="subtotal">
+                <h5>Shipping:</h5>
+                <h5>Flat rate: $3.00</h5>
+              </div>
+              <hr />
+              <div className="subtotal">
+                <h5>Total:</h5>
+                <h5>$19.48</h5>
+              </div>
+              <hr />
+              <div>
+                <button className="checkout">
+                  Proceed Checkout
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      </div>
-      {/* <Footer /> */}
+
+      <Footer />
     </>
   );
 };
 
 export default Cart;
+ 
