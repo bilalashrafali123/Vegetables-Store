9a2f79a945306410f8e7c187f481f5e062af1444
bilalashrafali123 <ashrafalibilal385@gmail.com>
2025-05-26 12:27:12 +0500

    updated...

--- src/components/Checkout/index.tsx
+++ src/components/Checkout/index.tsx
@@ -1,29 +1,57 @@
 import Header from '../Header'
 import Navbar from '../NavBar'
 import ShopBanner from '../ShopBanner'
+import "./index.scss"
 
 const Checkout = () => {
   return (
-  <>
+    <>
       <Header />
       <Navbar />
-      <ShopBanner title="Checkout" title2="Checkout" />
-      
-      <div className="container py-5">
-      <h1 className='checkout-heading'>Billing Address</h1>
-      <form action="#" className='form-section'>
-       <div className='row'>
-       <div className='left-section'>
-        <div className='row'>
-        <div className='inp'>
-            <label>First Name*</label>
-            <input type="text" />
-        </div>
-         
-        </div>
-       </div>
-       </div>
-      </form>
+      <ShopBanner title='Checkout' title2='Checkout' />
+      <div className="container">
+        <h1 className='billing'>Billing Details</h1>
+        <form className='form-section'>
+          <div className='input-section'>
+            <div className="input-group">
+              <label htmlFor="first-name">First Name *</label>
+              <input id="first-name" className='first-name' type="text" />
+            </div>
+            <div className="input-group">
+              <label htmlFor="last-name">Last Name *</label>
+              <input id="last-name" className='last-name' type="text" />
+            </div>
+          </div>
+
+          <div className="input-group full-width">
+            <label htmlFor="contact">Contact *</label>
+            <input id="contact" className='contact' type="text" />
+          </div>
+          <div className="input-group full-width">
+            <label htmlFor="contact">Address *</label>
+            <input id="contact" className='contact' type="text" placeholder='House number street name' />
+          </div>
+          <div className="input-group full-width">
+            <label htmlFor="contact">Town City *</label>
+            <input id="contact" className='contact' type="text" />
+          </div>
+          <div className="input-group full-width">
+            <label htmlFor="contact">Country *</label>
+            <input id="contact" className='contact' type="text" />
+          </div>
+          <div className="input-group full-width">
+            <label htmlFor="contact">Post Code Zip *</label>
+            <input id="contact" className='contact' type="text" />
+          </div>
+          <div className="input-group full-width">
+            <label htmlFor="contact">Mobile *</label>
+            <input id="contact" className='contact' type="text" />
+          </div>
+          <div className="input-group full-width">
+            <label htmlFor="contact">Email Address *</label>
+            <input id="contact" className='contact' type="text" />
+          </div>
+        </form>
       </div>
     </>
   )
